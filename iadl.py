1e656a43215a675d946cf2bb74a7e7051c452ef8
ZarkiSalleh <zarkisalleh1@gmail.com>
2026-03-28 17:08:22 +1100

    pull in old changes

--- iadl.py
+++ iadl.py
@@ -3,18 +3,26 @@ import DownloadItem
 import argparse
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    sub_parsers = parser.add_subparsers(dest="command")
-    sub_parsers.required = True
+def setup_arg_parser():
+    parser = argparse.ArgumentParser(description="Download items from Internet Archive")
+    parser.add_argument("-u", "--url", type=str, required=True, help="URL of the item to download")
+    parser.add_argument("-c", "--collection", type=str, required=True, help="Collection of the item")
+    parser.add_argument("-e", "--email", type=str, help="Email for authentication")
+    parser.add_argument("-p", "--password", type=str, help="Password for authentication")
+    parser.add_argument("-s", "--s3_access_key", type=str, help="S3 access key for authentication")
+    parser.add_argument("-k", "--s3_secret_key", type=str, help="S3 secret key for authentication")
+    parser.add_argument("-t", "--threads", type=int, default=1, help="Number of download threads")
 
-    parser_1 = sub_parsers.add_parser("download", help="download an item")
-    parser_1.add_argument("-f", "--file", action="store_true")
-    parser_1.add_argument("-s", "--sac", type=str, help="")
+    return parser
+
+
+def main():
+    parser = setup_arg_parser()
     args = parser.parse_args()
 
-    if args.command == "download":
-        pass
+    ia = InternetArchive.InternetArchive(
+        url=args.url, collection=args.collection, email=args.email, password=args.password
+    )
 
 
 if __name__ == "__main__":
--- testscript.py
+++ testscript.py
@@ -2,15 +2,32 @@ import json
 import os
 import DownloadItem
 import config
-import Archive 
+import InternetArchive
 
 
-item_metadata = Archive.get_metadata("COLLECTION_NAME")
-file = item_metadata["files"][0]
-url = f"https://archive.org/download/{item_metadata['metadata']['identifier']}/{file['name']}"
-a = Archive.authenticate(config.U, config.P)
-b = DownloadItem.DownloadItem(url, 10, 1048576, file['md5'], a)
-print(b.resource_location_url)
-b.download_resource()
-b.rebuild_resource()
-print(b.validate_resource_md5())
+ia = InternetArchive.InternetArchive(collection="redump_pc_N")
+ia.fetch_item_metadata()
+file = ia.metadata["files"][0]
+print(json.dumps(file, indent=2))
+if ia.is_file_private(file):
+    print("File is private, authentication required")
+    ia.authenticate(config.U, config.P)
+    file_url = f"https://archive.org/download/{ia.metadata['metadata']['identifier']}/{file['name']}"
+    dli = DownloadItem.DownloadItem(file_url, 10, 1048576, file["md5"], ia.s3_access_key, ia.s3_secret_key)
+    print(dli.resource_location_url)
+    dli.download_resource()
+    dli.rebuild_resource()
+    if dli.validate_resource_md5():
+        print("Resource validated")
+    else:
+        print("Resource validation failed, md5 mismatch")
+
+# item_metadata = Archive.get_metadata("COLLECTION_NAME")
+# file = item_metadata["files"][0]
+# url = f"https://archive.org/download/{item_metadata['metadata']['identifier']}/{file['name']}"
+# a = Archive.authenticate(config.U, config.P)
+# b = DownloadItem.DownloadItem(url, 10, 1048576, file["md5"], a)
+# print(b.resource_location_url)
+# b.download_resource()
+# b.rebuild_resource()
+# print(b.validate_resource_md5())
