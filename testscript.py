8e981bad43a2e48dad825c9024202e05b33eb38d
ZarkiSalleh <zarkisalleh1@gmail.com>
2026-03-28 17:55:11 +1100

    .

--- testscript.py
+++ testscript.py
@@ -1,26 +1,36 @@
-import json
-import os
-import DownloadItem
-import config
-import InternetArchive
+import HttpDownload
+
+import HttpDownload
+
+url = ""
+threads = 10
+chunk_size = 1048576
+md5 = ""
+dli = HttpDownload.HttpDownload(url, threads, chunk_size, md5)
+
+# import json
+# import os
+# import DownloadItem
+# import config
+# import InternetArchive
 
 
-ia = InternetArchive.InternetArchive(collection="redump_pc_N")
-ia.fetch_item_metadata()
-file = ia.metadata["files"][0]
-print(json.dumps(file, indent=2))
-if ia.is_file_private(file):
-    print("File is private, authentication required")
-    ia.authenticate(config.U, config.P)
-    file_url = f"https://archive.org/download/{ia.metadata['metadata']['identifier']}/{file['name']}"
-    dli = DownloadItem.DownloadItem(file_url, 10, 1048576, file["md5"], ia.s3_access_key, ia.s3_secret_key)
-    print(dli.resource_location_url)
-    dli.download_resource()
-    dli.rebuild_resource()
-    if dli.validate_resource_md5():
-        print("Resource validated")
-    else:
-        print("Resource validation failed, md5 mismatch")
+# ia = InternetArchive.InternetArchive(collection="redump_pc_N")
+# ia.fetch_item_metadata()
+# file = ia.metadata["files"][0]
+# print(json.dumps(file, indent=2))
+# if ia.is_file_private(file):
+#     print("File is private, authentication required")
+#     ia.authenticate(config.U, config.P)
+#     file_url = f"https://archive.org/download/{ia.metadata['metadata']['identifier']}/{file['name']}"
+#     dli = DownloadItem.DownloadItem(file_url, 10, 1048576, file["md5"], ia.s3_access_key, ia.s3_secret_key)
+#     print(dli.resource_location_url)
+#     dli.download_resource()
+#     dli.rebuild_resource()
+#     if dli.validate_resource_md5():
+#         print("Resource validated")
+#     else:
+#         print("Resource validation failed, md5 mismatch")
 
 # item_metadata = Archive.get_metadata("COLLECTION_NAME")
 # file = item_metadata["files"][0]
